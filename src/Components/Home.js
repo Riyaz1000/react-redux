2e8be43154f4ab12b47323e1d7002bfed2416a26
Riyaz H <hriyaz1412@gmail.com>
2025-05-02 20:04:31 +0530

    second commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,56 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import Contact from "./Contact";
 
 function Home() {
+  const [form, setForm] = useState([{
+    name: "",
+    age: "",
+    email: "",
+    contact: "",
+  }]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    console.log(name, value);
+
+    setForm((currentValue) => {
+
+      return  {
+
+      ...currentValue ,
+
+      [name] : value
+
+      }
+   
+    });
+
+ 
+  };
+
   return (
-
     <div>
-
-      
       <h1>Home component</h1>
 
       <div>
         <form>
-
           <label>Name :</label> <br />
-          <input type='text' />
-
+          <input
+            type="text"
+            name="name"
+            value={form.name}
+            style={{ outline: "none" }}
+            onChange={handleChange}
+          />
           <br /> <br />
-
-          <label>Age :</label>  <br />
-          <input type='number' />
-
+          <label>Age :</label> <br />
+          <input
+            type="number"
+            name="age"
+            value={form.age}
+            style={{ outline: "none" }}
+            onChange={handleChange}
+          />
           <br /> <br />
-
-          <label>E-mail :</label>  <br />
-          <input type='email' />
-
+          <label>E-mail :</label> <br />
+          <input
+            type="email"
+            name="email"
+            value={form.email}
+            style={{ outline: "none" }}
+            onChange={handleChange}
+          />
           <br /> <br />
-
-          <label>Contact :</label>  <br />
-          <input type='number' />
-
-
-
-
-
-
-
-
-
+          <label>Contact :</label> <br />
+          <input
+            type="number"
+            name="contact"
+            value={form.contact}
+            style={{ outline: "none" }}
+            onChange={handleChange}
+          />
+          <br /> <br />
+          <button>Submit</button>
         </form>
-
-        
       </div>
-
-
-      </div>
-
-
-
-       
-      
- 
-  )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/Components/User.js
+++ src/Components/User.js
@@ -1,10 +1,20 @@
-import React from 'react'
+
 
 function User() {
+
+
+
+
   return (
+
+
     <div>
 
         <h1>User Component</h1>
+
+        
+
+        
       
     </div>
   )
